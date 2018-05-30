325a4faa033c6833049ad33f9dab8777f4a4885d
Chau Nguyen <chau.nguyen@hotschedules.com>
2018-05-30 08:23:34 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,22 @@
 import '../css/app.scss';
 import M from 'materialize-css/dist/js/materialize.min';
+import ScrollReveal from 'scrollreveal/dist/scrollreveal.min';
 
 M.AutoInit();
 
-document.addEventListener('DOMContentLoaded', function() {
-  const heightWindow = window.innerHeight;
-  const nav = document.querySelector('.nav-vh');
+const functionSlider = (nav, heightWindow) => {
   const elems = document.querySelector('.carousel');
-  const carouselItems = document.querySelectorAll('.carousel-item');
-  const carouselImages = document.querySelectorAll('.carousel-item img');
   const minHeight = `${heightWindow - nav.offsetHeight}px`;
   elems.style.minHeight = minHeight;
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  const carouselImages = document.querySelectorAll('.carousel-item img');
   carouselItems.forEach((item) => {
     item.style.minHeight = minHeight;
   });
   carouselImages.forEach((img) => {
     img.onclick = function(e) {
       const href = e.target.getAttribute('attr-href');
-      // console.log(href, 'href');
+      window.open(href, '_blank');
     }
   });
   const options = {
@@ -27,10 +26,79 @@ document.addEventListener('DOMContentLoaded', function() {
     indicators: true
   };
   M.Carousel.init(elems, options);
-  // const instance = M.Carousel.getInstance(elems);
-  // setInterval(() => {
-  //   instance.next();
-  // }, 4800);
+  const instance = M.Carousel.getInstance(elems);
+  setInterval(() => {
+    instance.next();
+  }, 4800);
+};
+
+const scrollNav = (nav) => {
+  window.addEventListener('scroll', function() {
+    const { scrollY } = window;
+    if ((scrollY - 10) > nav.offsetHeight) {
+      nav.classList.add('nav-scroll');
+    } else {
+      nav.classList.remove('nav-scroll');
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  const heightWindow = window.innerHeight;
+  const nav = document.querySelector('.nav-vh');
+  functionSlider(nav, heightWindow);
+  scrollNav(nav);
 });
+
+
+const sr = new ScrollReveal({
+  viewFactor : 0.15,
+  duration   : 800,
+  distance   : "0px",
+  scale      : 0.8,
+});
+
+const configReveal = (origin= "top", distance= "50px", scale= 0) => ({
+  origin,
+  distance,
+  duration: 1200,
+  scale,
+});
+
+const titles = document.querySelectorAll('.title');
+titles.forEach((title) => {
+  sr.reveal(title, configReveal("bottom", "40px", .8));
+});
+
+const reasons = document.querySelectorAll('.reason-desc');
+reasons.forEach((reason, index) => {
+  if (index % 2 === 0) {
+    sr.reveal(reason, configReveal("left", "20px", .8));
+  } else {
+    sr.reveal(reason, configReveal("right", "20px", .8));
+  }
+});
+
+const financials = document.querySelectorAll('.financial-info');
+financials.forEach((financial) => {
+  sr.reveal(financial, configReveal("left", "40px", .7));
+});
+
+const clients = document.querySelectorAll('.clients-info');
+clients.forEach((client) => {
+  sr.reveal(client, configReveal("top", "40px", .7));
+});
+
+const gifts = document.querySelectorAll('.gift-info');
+gifts.forEach((gift) => {
+  sr.reveal(gift, configReveal("bottom", "40px", .7));
+});
+
+sr.reveal(".intro-desc", configReveal("top", "10px", .95));
+sr.reveal(".register-vh", configReveal("bottom", "20px", .8));
+
+
+
+
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,73 +4,110 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const extractPlugin = new ExtractTextPlugin({
-   filename: 'app.bundle.css'
+  filename: 'app.bundle.css'
 });
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/app.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ["stage-2"]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: extractPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.(jpg|jpeg|png|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'images/',
-                            publicPath: 'images/'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/',
-                            publicPath: 'fonts/'
-                        }
-                    }
-                ]
-            },
+  entry: ['babel-polyfill', './src/js/app.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['stage-2']
+            }
+          }
         ]
-    },
-    plugins: [
-        extractPlugin,
-        new HtmlWebpackPlugin({
-            title: 'NetLeaders',
-            hash: true,
-            template: 'src/index.html',
-        }),
-        new CleanWebpackPlugin(['dist'])
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: 'fonts/'
+            }
+          }
+        ]
+      },
     ]
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      title: 'Vinhomes Homepage',
+      inject: true,
+      hash: true,
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Vinhomes Products',
+      inject: true,
+      hash: true,
+      filename: 'products.html',
+      template: 'src/products.html',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Vinhomes Products Detail',
+      inject: true,
+      hash: true,
+      filename: 'products-details.html',
+      template: 'src/products-details.html',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Vinhomes Process',
+      inject: true,
+      hash: true,
+      filename: 'process.html',
+      template: 'src/process.html',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Vinhomes Process',
+      inject: true,
+      hash: true,
+      filename: 'process-details.html',
+      template: 'src/process-details.html',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Vinhomes Contacts',
+      inject: true,
+      hash: true,
+      filename: 'contacts.html',
+      template: 'src/contacts.html',
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
 };
