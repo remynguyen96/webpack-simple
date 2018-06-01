3ac49914feca406aeab758cb45a9b5c23ae97b0b
Chau Nguyen <chau.nguyen@hotschedules.com>
2018-06-01 09:08:33 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -3,101 +3,99 @@ import M from 'materialize-css/dist/js/materialize.min';
 import ScrollReveal from 'scrollreveal/dist/scrollreveal.min';
 
 M.AutoInit();
-
-const functionSlider = (nav, heightWindow) => {
-  const elems = document.querySelector('.carousel');
-  if (elems) {
-    const minHeight = `${heightWindow - nav.offsetHeight}px`;
-    elems.style.minHeight = minHeight;
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    const carouselImages = document.querySelectorAll('.carousel-item img');
-    carouselItems.forEach((item) => {
-      item.style.minHeight = minHeight;
-    });
-    carouselImages.forEach((img) => {
-      img.onclick = function(e) {
-        const href = e.target.getAttribute('attr-href');
-        window.open(href, '_blank');
-      }
-    });
-    const options = {
+const functionSlider = () => {
+  const carouselPrimary = document.querySelector('.slider-component > .carousel');
+  if (carouselPrimary) {
+    const carouselImages = document.querySelectorAll('.info-vh');
+    if (carouselImages.length) {
+      carouselImages.forEach((img) => {
+        img.onclick = (e) => {
+          const href = e.target.getAttribute('attr-href');
+          window.open(href, '_blank');
+        }
+      });
+    }
+    M.Carousel.init(carouselPrimary, {
       duration: 400,
       fullWidth: false,
       dist: 0,
       indicators: true
-    };
-    M.Carousel.init(elems, options);
-    const instance = M.Carousel.getInstance(elems);
+    });
+    const instance = M.Carousel.getInstance(carouselPrimary);
     setInterval(() => {
       instance.next();
     }, 4800);
   }
 };
-
-const scrollNav = (nav) => {
-  window.addEventListener('scroll', function() {
-    const { scrollY } = window;
-    if ((scrollY - 10) > nav.offsetHeight) {
-      nav.classList.add('nav-scroll');
-    } else {
-      nav.classList.remove('nav-scroll');
-    }
-  });
-};
-
-document.addEventListener('DOMContentLoaded', function() {
-  const heightWindow = window.innerHeight;
+const scrollNav = () => {
   const nav = document.querySelector('.nav-vh');
-  functionSlider(nav, heightWindow);
-  scrollNav(nav);
+  if (nav) {
+    window.addEventListener('scroll', () => {
+      const { scrollY } = window;
+      if ((scrollY - 10) > nav.offsetHeight) {
+        nav.classList.add('nav-scroll');
+      } else {
+        nav.classList.remove('nav-scroll');
+      }
+    });
+  }
+};
+document.addEventListener('DOMContentLoaded', function() {
+  functionSlider();
+  scrollNav();
+  /*Parallax*/
   const parallax = document.querySelector('.parallax');
-  M.Parallax.init(parallax, { responsiveThreshold: 5 });
+  if (parallax) {
+    M.Parallax.init(parallax, {});
+  }
+  /*Tab Detail*/
+  const tabs = document.querySelector('.tabs-detail');
+  const sliderTab = document.querySelector('.content.carousel');
+  if (tabs && sliderTab) {
+    M.Tabs.init(tabs, { swipeable: true });
+    M.Carousel.init(sliderTab, {
+      duration: 450,
+      fullWidth: true,
+      dist: 0,
+    });
+    const instance = M.Carousel.getInstance(sliderTab);
+    instance.dragged = true;
+    instance.pressed = true;
+  }
 });
-
+/*Scroll Page*/
 const sr = new ScrollReveal({
   viewFactor : 0.15,
   duration   : 800,
-  distance   : "0px",
-  scale      : 0.8,
+  distance   : "10px",
+  scale      : 0.85,
 });
-
 const configReveal = (origin= "top", distance= "50px", scale= 0) => ({
   origin,
   distance,
   duration: 1200,
   scale,
 });
-
 const titles = document.querySelectorAll('.title');
-titles.forEach((title) => {
-  sr.reveal(title, configReveal("bottom", "40px", .8));
-});
-
-const reasons = document.querySelectorAll('.reason-desc');
-reasons.forEach((reason, index) => {
-  if (index % 2 === 0) {
-    sr.reveal(reason, configReveal("left", "20px", .8));
-  } else {
-    sr.reveal(reason, configReveal("right", "20px", .8));
-  }
-});
-
+const reasons = document.querySelectorAll('.reason');
 const financials = document.querySelectorAll('.financial-info');
-financials.forEach((financial) => {
-  sr.reveal(financial, configReveal("left", "40px", .7));
-});
-
 const clients = document.querySelectorAll('.clients-info');
-clients.forEach((client) => {
-  sr.reveal(client, configReveal("top", "40px", .7));
-});
-
 const gifts = document.querySelectorAll('.gift-info');
-gifts.forEach((gift) => {
-  sr.reveal(gift, configReveal("bottom", "40px", .7));
+financials.forEach((_, index) => {
+  if (index % 2 === 0) {
+    sr.reveal(reasons[index], configReveal("left", "30px", .65));
+    sr.reveal(gifts[index], configReveal("top", "40px", .7));
+    sr.reveal(financials[index], configReveal("left", "40px", .7));
+  } else {
+    sr.reveal(reasons[index], configReveal("right", "30px", .65));
+    sr.reveal(gifts[index], configReveal("bottom", "40px", .7));
+    sr.reveal(financials[index], configReveal("right", "40px", .7));
+  }
+  sr.reveal(titles[index], configReveal("bottom", "40px", .8));
+  sr.reveal(clients[index], configReveal("top", "40px", .7));
 });
-
-sr.reveal(".intro-desc", configReveal("top", "10px", .95));
+sr.reveal(".intro-desc", configReveal("top", "10px", .8));
+sr.reveal(".youtube-vh", configReveal("bottom", "30px", .8));
 
 
 
