ab2956c4804fa61810ebbdf99ed77282926fd93f
Chau Nguyen <chauntn@smartosc.com>
2018-08-28 18:29:50 +0700

    WHO THE FUCK CAME UP WITH MAKE?

--- README.md
+++ README.md
@@ -1,9 +1,90 @@
-# webpack-simple made by Remy Nguyen
-This is webpack simple for frontend.
-* How to uses it ???
-  => It's very simple
-  Step 1: You need git clone (webpack for me)
-  Step 2: Run : npm install
-  Step 3: Run : npm run build or 
-Maybe you can run npm run build:prod to build folder dist. 
-Oh yeah enjoy it now !
+# Webpack-boilerplate made by Remy Nguyen
+# Webpack-boilerplate 
+## Overview
+
+* **Easiest way to run a native js:** Sensible defaults & includes everything you need with minimal setup.
+* **Compatible:** Works with all api servers (NodeJS, Java, PHP...) and fits seamless in your server workflow.
+
+`Webpack-boilerplate` is based on the following libraries & tools:
+
+* [`webpack`](https://webpack.js.org/): Performant, extensible web frontend framework
+
+## Features
+
+* ES6 typings
+* Scss
+* HTML
+* Accepts `application/json` content-types
+* Runs everywhere: Can be deployed via `now`, `up`, AWS Lambda, Heroku, Firebase, Digitalocean etc.
+
+
+## Getting Started
+***
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+- Install **Node LTS 8** 
+  - Download: https://nodejs.org/en/download/
+
+
+### Installation
+
+  Install yarn as global packages if not ``` npm install -g yarn ```
+```
+    yarn install
+```
+*You can use npm. But I recommend usage yarn instend of npm*
+
+### Usage
+***
+
+#### Running application
+
+```
+  yarn start
+```
+
+Server will start with port http://localhost:3000
+
+> By default application will run on port 3000. If you want to specify port, ex: **3500** , on Windows you modify **scripts** part of **package.json** file as below:
+>  - **Current**: ``` "start": "webpack-dev-server --open --port 3000" ```  
+>  - **Change to**: ``` "start": "webpack-dev-server --open --port 3500" ```
+
+
+#### Running the tests
+
+```
+  yarn test (Coming soon)
+```
+
+### Build
+
+```
+  yarn build
+```
+
+## Deployment with Firebase
+```
+  yarn deploy
+```
+
+### Heroku
+
+To deploy your application server with [Heroku](https://heroku.com), follow these instructions:
+
+1.  Download and install the [Heroku Command Line Interface](https://devcenter.heroku.com/articles/heroku-cli#download-and-install) (previously Heroku Toolbelt)
+2.  Log in to the Heroku CLI with `heroku login`
+3.  Navigate to the root directory of your `app` server
+4.  Create the Heroku instance by executing `heroku create`
+5.  Deploy your app server by executing `git push heroku master`
+
+### `up` (Coming soon)
+
+### AWS Lambda (Coming soon)
+
+## Help & Community
+
+Join me. if you run into issues or have questions. I love talking to you!
+
+<p align="center"><a href="https://oss.prisma.io"><img src="https://imgur.com/IMU2ERq.png" alt="Prisma" height="170px"></a></p>
--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,13 @@ import ko from 'knockout';
 // import $ from 'jquery';
 // $(document).ready(function () {
 
+
+/**
+ * @Description: Part 1 Knockout JS
+ * @Author: Chau Nguyen
+ */
 function MyViewModel() {
   const self = this;
-
   this.changeKey = ko.observable('Enter ...').extend({
     notify: 'always'
   });
@@ -17,12 +21,9 @@ function MyViewModel() {
   this.textTransform = function () {
     return this.changeKey().toLowerCase();
   };
-
-  trackingDependencies(self);
-
-  delayLog(self);
-
-
+  
+  // trackingDependencies(self);
+  // delayLog(self);
   appData(self);
 }
 
@@ -34,12 +35,12 @@ function trackingDependencies(self) {
       id: self.idUser(),
       selected: self.selectedUser.peek(),
     };
-    console.log(params, 'params');
-    fetch(`https://jsonplaceholder.typicode.com/users/${params.selected}`)
+    // console.log(params, 'params');
+    fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
       .then(res => res.json())
       .then(json => console.log(json, 'json'));
     return self.changeKey().toLowerCase();
-  }, self).extend({ rateLimit: { timeout: 2000, method: "notifyWhenChangesStop" } });
+  }, self).extend({ rateLimit: { timeout: 1000, method: "notifyWhenChangesStop" } });
   self.nextUser = () => {
     return self.selectedUser(self.selectedUser() + 1);
     // return self.idUser(self.idUser() + 1);
@@ -49,7 +50,7 @@ function trackingDependencies(self) {
 function delayLog(self) {
   self.instantaneousValue = ko.observable();
   self.delayedValue = ko.pureComputed(self.instantaneousValue).extend({
-    rateLimit: { method: "notifyWhenChangesStop", timeout: 500 }
+    rateLimit: { method: "notifyWhenChangesStop", timeout: 1000 }
   });
   self.loggedValues = ko.observableArray([]);
   self.delayedValue.subscribe((value) => {
@@ -68,11 +69,16 @@ function appData(self) {
     self.computedLog(`${self.computedLog.peek()} ${valueName};`);
     return valueName;
   }, self);
+  // self.fullName.subscribe((value) => {
+  // console.log(value, 'start');
+  // }, self, 'awake');
+  // }, self, 'asleep');
   self.step = ko.observable(0);
   self.nextStep = () => {
     self.step(self.step() === 2 ? 0 : self.step() + 1);
   };
 }
+
 
 // const myObject = new MyViewModel();
 // let result = {};
@@ -83,8 +89,87 @@ function appData(self) {
 // }
 // console.log(result);
 
+// ko.applyBindings(new MyViewModel());
+/**
+ * @Description: Part 2 Knockout JS
+ * @Author: Chau Nguyen
+ */
 
-ko.applyBindings(new MyViewModel());
+function SeatReservation(name, initialMeal) {
+  const self = this;
+  self.name = name;
+  self.meal = ko.observable(initialMeal);
+  self.formattedPrice = ko.computed(() => {
+    const { price } = self.meal();
+    return price ? `$ ${price.toFixed(2)}` : 'None';
+  });
+}
 
+function ReservationsViewModel() {
+  const self = this;
+  self.isEmpty = (object) => {
+    for (let key in object) {
+      if (object.hasOwnProperty(key))
+        return false;
+    }
+    return true;
+  };
+  
+  self.availabelMeals = [
+    { mealName: 'Standar (sandwich)', price: 0 },
+    { mealName: 'Premium (lobster)', price: 34.95 },
+    { mealName: 'Ultimate (whole zebra)', price: 105 },
+  ];
+  
+  self.seats = ko.observableArray([
+    new SeatReservation('Steve', self.availabelMeals[1]),
+    new SeatReservation('Bert', self.availabelMeals[2]),
+  ]);
+  
+  self.addSeat = () => {
+    self.seats.push(new SeatReservation('name...', self.availabelMeals[0]))
+  };
+  
+  self.totalSurcharge = ko.pureComputed(() => {
+    let total = 0;
+    // for (let i = 0; i < self.seats().length; i++)
+    //   total += self.seats()[i].meal().price;
+    self.seats().forEach(seat => total += seat.meal().price);
+    return total;
+  });
+  
+  self.removeSeat = (seat) => {
+    console.log(seat, 'seat');
+    self.seats.remove(seat);
+  };
+  
+  self.result = ko.observable({});
+  
+  self.orderReservation = () => {
+    const result = {
+      name: '',
+      totalSurcharge: self.totalSurcharge(),
+      seats: [],
+      totalSeats: 0,
+    };
+    self.seats().forEach((item) => {
+      const { name, meal } = item;
+      result.name = name;
+      result.seats = [...result.seats, meal()];
+    });
+    self.seats.removeAll();
+    self.result(result);
+    return result;
+  };
+}
+
+// ko.applyBindings(new ReservationsViewModel());
+
+/**
+ * @Description: Part 3 Knockout JS
+ * @Author: Chau Nguyen
+ */
+
+// ko.applyBindings(new ReservationsViewModel());
 
 // });
